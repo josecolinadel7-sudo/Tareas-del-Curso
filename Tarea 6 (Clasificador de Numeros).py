75eac31ce82f07e26244cb8a44a6dd3abd655a52
josecolinadel7-sudo <josecolinadel7@gmail.com>
2025-08-19 11:40:06 -0400

    Delete Tarea 6 (Clasificador de Numeros).py

--- Tarea 6 (Clasificador de Numeros).py
+++
@@ -1,9 +0,0 @@
-# Clasificacion de Numero
-numero = float(input("Ingresa un número: "))
-
-if numero > 0:
-    print("El número es POSITIVO")
-elif numero < 0:
-    print("El número es NEGATIVO")
-else:
-    print("El número es CERO ")
