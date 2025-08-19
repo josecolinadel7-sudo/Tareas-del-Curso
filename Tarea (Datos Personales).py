db02963d3febeda54d9d33051aeb823bd47e722f
josecolinadel7-sudo <josecolinadel7@gmail.com>
2025-08-19 11:38:19 -0400

    Delete Tarea (Datos Personales).py

--- Tarea (Datos Personales).py
+++
@@ -1,6 +0,0 @@
-Saludo = "Holaa"
-Nombre = input("Por favor, ingrese su nombre: ")
-Edad =  input("Por favor, ingrese su Edad: ")
-Altura =  input("Por favor, ingrese su Altura: ")
-Lugar_de_Nacimiento = input("Por favor, ingrese ciudad de nacimiento: ")
-print(f"\n{Saludo} me llamo {Nombre}, Tengo {Edad}, actualmente mido {Altura}cm, y naci en la ciudad de {Lugar_de_Nacimiento}")
