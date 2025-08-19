75065e6dde4d4ed9c2396eb6013e9217cea18ecf
josecolinadel7-sudo <josecolinadel7@gmail.com>
2025-08-19 11:39:09 -0400

    Delete Tarea 3 (Temperatura).py

--- Tarea 3 (Temperatura).py
+++
@@ -1,4 +0,0 @@
-# Tarea 3 (Temperatura)
-celsius = float(input("Ingresa la temperatura en Celsius: "))
-fahrenheit = (celsius * 9/5) + 32 
-print(f"{celsius}ºCelsius equivale a {(fahrenheit)}ºFahrenheit")
