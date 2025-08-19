f2d59680cb670db8d39383994de0fbfc8da9bc10
josecolinadel7-sudo <josecolinadel7@gmail.com>
2025-08-19 11:39:55 -0400

    Delete Tarea 5 (Calcular base y perimetro).py

--- Tarea 5 (Calcular base y perimetro).py
+++
@@ -1,8 +0,0 @@
-
-print("Vamos a calcular el area y perimetro de un Rectangulo")
-base= float(input("Ingrese la base del rectangulo: "))
-altura = float(input("Ingrese la altura del rectangulo: ")) 
-area = base * altura
-perimetro = 2 * (base + altura)
-print(f"El area del rectangulo es: {area}")
-print(f"El perimetro del rectangulo es: {perimetro}")
